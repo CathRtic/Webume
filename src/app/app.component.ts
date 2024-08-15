8bc6de8fcd4dfd8874d7804a835cacde19b765ed
CathRtic <nikunj2077@outlook.com>
2024-08-15 02:20:51 -0400

    Resume Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from "./components/nav/nav.component";
-import { ModalModule } from 'ngx-bootstrap/modal';
+
 
 @Component({
   selector: 'app-root',
--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-  constructor(private titleService: Title){
+  constructor(private titleService: Title, private renderer: Renderer2){
     this.titleService.setTitle('Nikunj Patel - Resume')
   }
 
+  DownloadPDF(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'Resume.pdf');
+    link.setAttribute('download', 'Resume.pdf');
+    link.click();
+    link.remove();
+  }
 }
+
