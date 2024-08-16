fed077d3bbc971f6d5900347cf12145a2c9aba0f
CathRtic <nikunj2077@outlook.com>
2024-08-15 23:18:16 -0400

    Interactive Resume Styling

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,16 +1,23 @@
 import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [AccordionModule],
+  imports: [AccordionModule, NgIf],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
+
+  IsWorkExperienceOpen: boolean = false;
+  IsEducationOpen: boolean = false;
+  IsCertificationsOpen: boolean = false;
+  IsSkillsOpen: boolean = false;
+
   constructor(private titleService: Title, private renderer: Renderer2){
     this.titleService.setTitle('Nikunj Patel - Resume')
   }
